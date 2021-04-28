08a5dd1f4e70a82595b1cb9e58f20338d7444b3d
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-04-28 20:48:32 +0800

    init

---
+++ src/main/java/cn/com/xuxiaowei/SpringBootOriginalWebservicesApplication.java
@@ -0,0 +1,18 @@
+package cn.com.xuxiaowei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 程序执行入口
+ *
+ * @author 徐晓伟
+ */
+@SpringBootApplication
+public class SpringBootOriginalWebservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOriginalWebservicesApplication.class, args);
+    }
+
+}
